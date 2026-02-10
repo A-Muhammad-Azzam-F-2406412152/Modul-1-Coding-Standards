b456b020274221bceac7c7ea97dace8fe8dcda3c
azzzfath <AzzamFathurrahman23@gmail.com>
2026-02-10 15:32:30 +0700

    gradle

--- src/main/java/id/cs/ui/ac/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/cs/ui/ac/advprog/eshop/controller/ProductController.java
@@ -40,7 +40,7 @@ public class ProductController {
     public String editProductPage(@PathVariable("id") String id, Model model) {
         Product product = service.findById(id);
         model.addAttribute("product", product);
-        return "editProduct"; // Kamu perlu buat file editProduct.html nanti
+        return "editProduct";
     }
 
     @PostMapping("/edit")
